2acc5d89a57b90c6e5da2922f5e2678b20c87e22
Mike7R <mice7r@gmail.com>
2017-05-29 16:00:48 +0200

    All csv in one list

--- read.r
+++ read.r
@@ -18,15 +18,22 @@ leerwincsv <- function(file) {
   return(r);
 }
 
-seguridad <- leerwincsv("Seguridad.csv")
-sistemas <- leerwincsv("Sistema.csv")
-instalacion <- leerwincsv("Instalacion.csv")
-aplicacion <- leerwincsv("Aplicacion.csv")
+readbunch <- function(files) {
+    names <- files
+    mylist <- list()
+    for (i in seq_along(files)) {
+        mylist[[i]] <- leerwincsv(files[i])
+        attr(mylist[[i]], "name") <- names[i] # Add a name to the dataframe: Nice trick for later
+    }
+    return(mylist)
+}
 
+CSV <- readbunch(c("Seguridad.csv", "Sistema.csv", "Instalacion.csv", "Aplicacion.csv"))
+# Nos los guardamos en una lista para trabajar mejor con ellos
 
 print("plot sizes.png")
 png(filename="sizes.png")
-barplot(c(nrow(seguridad), nrow(sistemas), nrow(instalacion), nrow(aplicacion)), names.arg=c("Seguridad", "Sistemas", "Instalacion", "Aplicacion"))
+barplot(sapply(CSV,(function(x) nrow(x))), names.arg=sapply(CSV,(function(x) attr(x, "name", exact=T))))
 dev.off()
 
 library("dplyr")
@@ -35,15 +42,16 @@ id_barplot <- function(df) {
   df.count <- count(df, Id) # contar la cantidad de eventos por Id
   ylim <- c(0, 1.1*max(df.count$n))
   x <- df.count[order(df.count$n, decreasing=T),] # df.count ordenado
-  xx <- barplot(x$n, main="LOL", ylab="Cantidad", ylim = ylim, cex.axis = 1.2)
+  xx <- barplot(x$n, main=attr(df, "name", exact=T), ylab="Cantidad", ylim = ylim, cex.axis = 1.2)
   text(x = xx, y = x$n, label = x$n, pos = 3, cex = 1.0, col = "red")
   axis(1, at = xx, labels = x$Id, tick = F, las = 2, line = -0,5, cex.axis=1.2)
 }
 
 png(filename="id.count.png", width=1920, 1080)
 par(mfrow=c(2,2))
-id_barplot(sistemas) # FIXME: Demasiados id
-id_barplot(instalacion) # FIXME: los id los pone entre las barras
-id_barplot(seguridad)
-id_barplot(aplicacion) # FIXME: Demasiados id
+lapply(CSV, id_barplot)
+#id_barplot(sistemas) # FIXME: Demasiados id
+#id_barplot(instalacion) # FIXME: los id los pone entre las barras
+#id_barplot(seguridad)
+#id_barplot(aplicacion) # FIXME: Demasiados id
 dev.off()

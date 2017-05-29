c43b726b01f921799c8e4f1f59636afb365c50ea
Mike7R <mice7r@gmail.com>
2017-05-29 18:56:31 +0200

    TOP3

--- read.r
+++ read.r
@@ -55,3 +55,17 @@ lapply(CSV, id_barplot)
 #id_barplot(seguridad)
 #id_barplot(aplicacion) # FIXME: Demasiados id
 dev.off()
+
+png(filename="id.count.top3.png", width=1920, 1080)
+par(mfrow=c(2,2))
+lapply(CSV, function(df) { 
+  df.count <- count(df, Id) # contar la cantidad de eventos por Id
+  ylim <- c(0, 1.1*max(df.count$n))
+  x <- df.count[order(df.count$n, decreasing=T),] # df.count ordenado
+  x <- x[1:3,] # TOP 3
+  xx <- barplot(x$n, main=attr(df, "name", exact=T), ylab="Cantidad", ylim = ylim, cex.axis = 1.2)
+  text(x = xx, y = x$n, label = x$n, pos = 3, cex = 1.0, col = "red")
+  axis(1, at = xx, labels = x$Id, tick = F, las = 2, line = -0,5, cex.axis=1.2)
+})
+dev.off()
+
